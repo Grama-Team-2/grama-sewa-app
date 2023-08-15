21119da569cf31cc6863f710c127cab153e2641e
Zeta201 <dini2040gh@gmail.com>
2023-08-15 14:28:31 +0530

    webhook

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -12,9 +12,14 @@ import { AuthProvider } from "@asgardeo/auth-react";
 const config = {
   signInRedirectURL: "http://localhost:3000",
   signOutRedirectURL: "http://localhost:3000",
-  clientID: "X7fb7Ie9feZyVZi2ouH6MbNvGV8a",
+  clientID: "sIUjQFULB4OeAVq0vPofXGwyBo0a",
   baseUrl: "https://api.asgardeo.io/t/interns",
-  scope: ["openid", "profile"],
+  scope: [
+    "openid",
+    "profile",
+    "app_roles",
+    "urn:interns:gramaapiservicerequests91:reqeust_reaction urn:interns:gramaapiservicerequests91:get_all_requests urn:interns:gramaapiservicerequests91:post_a_request urn:interns:identitycheckserviceident:check_identity urn:interns:policecheckservicepolicev:police_ver_check urn:interns:addresscheckserviceaddres:check_address",
+  ],
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
