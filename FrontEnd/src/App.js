b3460cd025df03b97546ec4f2a068996866f2a91
Dharane-Segar <dharanesegar08@gmail.com>
2023-08-17 15:06:20 +0530

    Changed user Requests

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -39,7 +39,7 @@ function App() {
   useEffect(() => {
     getBasicInfo();
 
-    navigate("/");
+    
   }, [role]);
 
   return (
--- FrontEnd/src/api/UserRequests.js
+++ FrontEnd/src/api/UserRequests.js
@@ -8,4 +8,16 @@ export const newRequest = {
   url: "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/maoe/grama-api-service/requests-915/1.0.0/newRequest",
   
 };
+
+export const checkStatus = {
+  headers: {
+    Accept: "application/json",
+  },
+  attachToken: true,
+  withCredentials: true,
+  method: "GET",
+  url: "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/maoe/grama-api-service/requests-915/1.0.0/getStatus",
+  
+};
+
 //https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/maoe/slacknotifications/endpoint-7070-070/1.0.0
