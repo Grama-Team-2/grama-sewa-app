54a30db225191ef03e525415783d1da68770d595
NirashaSewwandi <dwsnsewwandi@gmail.com>
2023-08-23 13:43:43 +0530

    modify UI

--- FrontEnd/src/Components/User/UserDashboard.js
+++ FrontEnd/src/Components/User/UserDashboard.js
@@ -10,7 +10,7 @@ function UserDashBoard() {
     <main >
         
     <div className="desktop" style={{backgroundColor:"grey",width:"100%",margin: 0,padding: 0,justifyContent:"center"}}>
-      <div className="overlap"style={{backgroundColor:"grey",width:"100%",alignItems: "center",padding: 0,marginRight:-100,marginLeft:-100}}>
+      <div className="overlap"style={{backgroundColor:"grey",width:"100%",alignItems: "center",padding: 0,marginRight:-100,marginLeft:-150}}>
         <div className="frame"style={{alignItems:"center"}}>
           <img
                 className="logo"
@@ -20,8 +20,9 @@ function UserDashBoard() {
                 
                 src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
               />
+              <div className="text-wrapper" style={{marginTop:"50px"}}>GRAMA ASSIST</div>
             </div>
-            <div className="text-wrapper" style={{marginTop:"50px"}}>GRAMA ASSIST</div>
+            
 
           </div>
           
