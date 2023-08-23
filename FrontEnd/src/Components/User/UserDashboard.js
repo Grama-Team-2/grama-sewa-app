43c4a875c35f7dcc16bb56194a0a861393769375
NirashaSewwandi <dwsnsewwandi@gmail.com>
2023-08-23 13:19:25 +0530

    fix

--- FrontEnd/src/Components/User/UserDashboard.js
+++ FrontEnd/src/Components/User/UserDashboard.js
@@ -7,15 +7,21 @@ function UserDashBoard() {
   return (
     <>
     
-    <main>
-        <div className="dashboard-container">
-          <div className="logo-wrapper">
-            <img
-              className="logo"
-              alt="Logo"
-              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-            />
-            
+    <main >
+        
+        <div className={`desktop`}>
+          <div className="overlap">
+            <div className="frame">
+              <img
+                className="logo"
+                alt="Logo"
+                style={{marginTop:"70px"}}
+                
+                
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
+              />
+            </div>
+            <div className="text-wrapper" style={{marginTop:"50px"}}>GRAMA ASSIST</div>
 
           </div>
           
